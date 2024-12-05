bde7fdafae0c56db1d8c31e4b4b308473e1dfe7b
tomoto-png <hirosi.tomoto0130@gmail.com>
2024-12-05 19:55:15 +0900

    Menuモデルとテーブルの修正ができましたお願いします

--- laravel-app/database/migrations/2024_12_05_103109_create_menus_table.php
+++ laravel-app/database/migrations/2024_12_05_103109_create_menus_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('menus', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger("company_id");
             $table->string("name");
             $table->timestamps();
         });
