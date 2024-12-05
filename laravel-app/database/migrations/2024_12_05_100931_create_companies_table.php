4c3fe0399e0b604fa9f393be0b82149415bad7cb
tomoto-png <hirosi.tomoto0130@gmail.com>
2024-12-05 19:29:29 +0900

    Companyのテーブルを直しました

--- laravel-app/database/migrations/2024_12_05_100931_create_companies_table.php
+++ laravel-app/database/migrations/2024_12_05_100931_create_companies_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
-            $name->string();
+            $table->string("name");
             $table->timestamps();
         });
     }
