cffa7d081c3346e647f270b37a770feb3a7a70c1
tomoto-png <hirosi.tomoto0130@gmail.com>
2024-12-09 15:14:14 +0900

    リレーションができました

--- laravel-app/app/Models/Menu.php
+++ laravel-app/app/Models/Menu.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Menu extends Model
 {
     protected $fillable = ["company_id","name"];
+
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
+    public function menuPages(): HasMany
+    {
+        return $this->hasMany(MenuPage::class);
+    }
 }
--- laravel-app/app/Models/MenuItem.php
+++ laravel-app/app/Models/MenuItem.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class MenuItem extends Model
 {
     protected $fillable = ["merch_id","manu_page_id","width","height","top","left"];
+
+    public function menuPage(): BelongsTo
+    {
+        return $this->belongsTo(MenuPage::class);
+    }
+    public function merche(): BelongsTo
+    {
+        return $this->belongsTo(Merche::class);
+    }
 }
