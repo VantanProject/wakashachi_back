4e815316909541463e1d331738d82777248db937
kaito0523 <142303414+kaito0523@users.noreply.github.com>
2024-12-12 16:41:16 +0900

    update api.php

--- laravel-app/routes/api.php
+++ laravel-app/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Model\Allergy;
 use App\Http\Controllers\MerchController;
 
 Route::get('/user', function (Request $request) {
@@ -9,3 +10,16 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::apiResource('merch', MerchController::class);
+
+Route::get("/allergy/select", function() {
+    $allergies = Allergy::get()->map(function ($query) {
+        return [
+            "value" => $query->id,
+            "label" => $query->name,
+        ];
+    });
+    return response()->json([
+        "success" => true,
+        "allergies" => $allergies,
+    ]);
+});
