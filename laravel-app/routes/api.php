39bb1aeb1a3c9055aed9d361a2c21bb697c47b2c
福品悠翔 <haruto.fukushina0207@gmail.com>
2024-12-14 21:47:56 +0900

    Feature/fuku/login (#16)
    * seederを取り入れるため
    
    * ログイン機能

--- laravel-app/routes/api.php
+++ laravel-app/routes/api.php
@@ -2,12 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Models\Allergy;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::post('/login', [AuthController::class, 'index']);
 Route::get("/allergy/select", function() {
     $allergies = Allergy::get()->map(function ($query) {
         return [
