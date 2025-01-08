d04c48d5a50965217d6d105b25f7d2fc493475cb
tomoto-png <hirosi.tomoto0130@gmail.com>
2025-01-08 19:31:32 +0900

    text_translationsテーブルを追加しました。textカラムを削除しました

--- laravel-app/database/migrations/2024_12_17_071415_create_menu_image_translations_table.php
+++ laravel-app/database/migrations/2024_12_17_071415_create_menu_image_translations_table.php
@@ -11,10 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('menu_item_texts', function (Blueprint $table) {
+        Schema::create('menu_image_translations', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('menu_item_id');
-            $table->string('color');
+            $table->unsignedBigInteger('menu_page_id');
+            $table->unsignedBigInteger('language_id');
+            $table->string('img_url');
             $table->timestamps();
         });
     }
@@ -24,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('menu_item_texts');
+        Schema::dropIfExists('menu_image_translations');
     }
 };
